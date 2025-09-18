026806147da536a374c78a1b06abcb6f2b9e443b
andi-hbtn <andi.bevapi@holbertonschool.com>
2025-09-18 12:54:37 +0200

    notifications.spec.js

---
+++ react_intro/task_3/dashboard/src/Notifications.spec.js
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import Notifications from "./Notifications.jsx";
+
+test("Notifications.jsx test", () => {
+    render(<Notifications />);
+    const notificationTitle = screen.getByText('Here is the list of notifications');
+    expect(notificationTitle).toBeInTheDocument();
+
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
+
+    const listItems = screen.getAllByRole('listitem');
+    expect(listItems).toHaveLength(3)
+
+    // 1. Krijojmë një spy për console.log
+    const consoleSpy = jest.spyOn(console, 'log').mockImplementation(() => { });
+
+    // 2. Marrim butonin sipas aria-label="Close"
+    const closeButton = screen.getByRole('button', { name: /close/i });
+
+    //Simulojme nje klikim te butonit
+    fireEvent.click(closeButton);
+
+    // 3. Verifikojmë që console.log është thirrur me tekstin e duhur
+    expect(consoleSpy).toHaveBeenCalledWith("Close button has been clicked");
+
+    // 4. Pastrimi i spy për të mos ndikuar testet e tjera
+    consoleSpy.mockRestore();
+
+});
