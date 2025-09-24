26689eb381d76cd40bca1dda7f2369eb425a8504
andi-hbtn <andi.bevapi@holbertonschool.com>
2025-09-24 17:43:20 +0200

    props

--- react_props/task_0/dashboard/src/Footer/Footer.jsx
+++ react_props/task_0/dashboard/src/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Footer.css';
-import { getFooterCopy, getFullYear } from '../utils/util';
+import { getFooterCopy, getFullYear } from '../utils/utils';
 
 function Footer() {
     return (
