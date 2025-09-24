942b026b2f57c1ffe9dbe119894fc76110e83824
andi-hbtn <andi.bevapi@holbertonschool.com>
2025-09-24 17:24:26 +0200

    props

--- react_props/task_0/dashboard/src/utils/util.spec.js
+++ react_props/task_0/dashboard/src/utils/util.spec.js
@@ -1,21 +1,21 @@
-import { getFullYear, getFooterCopy, getLatestNotification }from './util';
+import { getFullYear, getFooterCopy, getLatestNotification } from './util';
 
 describe('utilsTest', () => {
     describe('yearTest', () => {
-        it ('should return the same year', () => {
+        it('should return the same year', () => {
             expect(getFullYear()).toEqual(new Date().getFullYear());
         });
     });
     describe('footerTest', () => {
-        it ('should return the correct string when argument is true', () => {
+        it('should return the correct string when argument is true', () => {
             expect(getFooterCopy(true)).toEqual('Holberton School');
         });
-        it ('should return the correct string when argument is false', () => {
+        it('should return the correct string when argument is false', () => {
             expect(getFooterCopy(false)).toEqual('Holberton School main dashboard');
         });
     });
     describe('notificationTest', () => {
-        it ('should check the returned string', () => {
+        it('should check the returned string', () => {
             expect(getLatestNotification()).toEqual('<strong>Urgent requirement</strong> - complete by EOD');
         });
     });
