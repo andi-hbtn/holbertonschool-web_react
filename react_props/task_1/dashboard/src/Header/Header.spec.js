1289b63a2bff85dc2a0e3a8cf8666799f62207eb
andi-hbtn <andi.bevapi@holbertonschool.com>
2025-09-24 18:00:09 +0200

    props

--- react_props/task_1/dashboard/src/Header/Header.spec.js
+++ react_props/task_1/dashboard/src/Header/Header.spec.js
@@ -1,25 +1,15 @@
-import Header from './Header';
+import React from 'react';
 import { shallow } from 'enzyme';
+import Header from './Header';
 
-describe('Testing <Header /> component', () => {
-    describe('Header renders without crashing', () => {
-        it ('should render Header withour crashing', () => {
-            const wrapper = shallow(<Header />);
-            expect(wrapper.exists()).toEqual(true);
-        });
+describe('Header component', () => {
+    it('should render the Holberton logo', () => {
+        const wrapper = shallow(<Header />);
+        expect(wrapper.find('img').prop('alt')).toBe('Holberton logo');
     });
-    describe('Header renders an img element', () => {
-        it ('should render an img in Header component', () => {
-            const wrapper = shallow(<Header />);
-            wrapper.update();
-            expect(wrapper.find('img')).toHaveLength(1);
-        });
-    });
-    describe('Header renders a h1 element', () => {
-        it ('should render a h1 in Header component', () => {
-            const wrapper = shallow(<Header />);
-            wrapper.update();
-            expect(wrapper.find('h1')).toHaveLength(1);
-        });
+
+    it('should render the heading with the correct text', () => {
+        const wrapper = shallow(<Header />);
+        expect(wrapper.find('h1').text()).toBe('School dashboard');
     });
 });
