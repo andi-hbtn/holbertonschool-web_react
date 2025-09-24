67bc3c626a724cf6ddefaa9aeb53b2a1f28b74b1
andi-hbtn <andi.bevapi@holbertonschool.com>
2025-09-24 18:03:28 +0200

    props

--- react_props/task_1/dashboard/src/Header/Header.spec.js
+++ react_props/task_1/dashboard/src/Header/Header.spec.js
@@ -3,13 +3,15 @@ import { shallow } from 'enzyme';
 import Header from './Header';
 
 describe('Header component', () => {
-    it('should render the Holberton logo', () => {
+    it('renders the Holberton logo', () => {
         const wrapper = shallow(<Header />);
-        expect(wrapper.find('img').prop('alt')).toBe('Holberton logo');
+        const img = wrapper.find('img');
+        expect(img.prop('alt')).toBe('Holberton logo');
     });
 
-    it('should render the heading with the correct text', () => {
+    it('renders the heading text', () => {
         const wrapper = shallow(<Header />);
-        expect(wrapper.find('h1').text()).toBe('School dashboard');
+        const heading = wrapper.find('h1');
+        expect(heading.text()).toBe('School dashboard');
     });
 });
