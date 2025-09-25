21223948c8880fdd041b8df9b6cb8ce8a3f762cd
andi-hbtn <andi.bevapi@holbertonschool.com>
2025-09-25 13:42:06 +0200

    rename the file

--- react_props/task_0/dashboard/src/App/App.jsx
+++ react_props/task_0/dashboard/src/App/App.jsx
@@ -1,4 +1,6 @@
+// App.jsx
 import './App.css';
+import { Fragment } from 'react';
 import Notifications from "../Notifications/Notifications.jsx";
 import Header from '../Header/Header.jsx';
 import Login from '../Login/Login.jsx';
@@ -10,15 +12,16 @@ function App() {
     { id: 2, type: 'urgent', value: 'New resume available' },
     { id: 3, type: 'urgent', html: { __html: 'Urgent requirement - complete by EOD' } },
   ];
+
   return (
-    <>
+    <Fragment>
       <Notifications notifications={notificationsList} />
       <div className="App">
         <Header />
         <Login />
         <Footer />
       </div>
-    </>
+    </Fragment>
   );
 }
 
--- react_props/task_0/dashboard/src/Login/Login.jsx
+++ react_props/task_0/dashboard/src/Login/Login.jsx
@@ -1,3 +1,4 @@
+// Login.jsx
 import './Login.css';
 
 function Login() {
