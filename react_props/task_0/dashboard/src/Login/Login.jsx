53212442b79b831a34f7cf7691d332170214253d
andi-hbtn <andi.bevapi@holbertonschool.com>
2025-09-25 13:39:20 +0200

    rename the file

--- react_props/task_0/dashboard/src/Login/Login.jsx
+++ react_props/task_0/dashboard/src/Login/Login.jsx
@@ -2,13 +2,13 @@ import './Login.css';
 
 function Login() {
     return (
-        <div className="App-body">
+        <div className="Login">
             <p>Login to access the full dashboard</p>
             <label htmlFor="email">Email: </label>
             <input type="email" name="email" id="email" />
             <label htmlFor="password">Password: </label>
             <input type="password" name="password" id="password" />
-            <button type="button">OK</button>
+            <button type="submit">OK</button>
         </div>
     );
 }
