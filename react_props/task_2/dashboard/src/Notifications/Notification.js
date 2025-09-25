6db7200e98999ce6bf88cff73819037156f25791
andi-hbtn <andi.bevapi@holbertonschool.com>
2025-09-25 12:37:18 +0200

    changes for test

--- react_props/task_2/dashboard/src/Notifications/Notification.js
+++ react_props/task_2/dashboard/src/Notifications/Notification.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import './Notification.css';
-import closeIcon from '../assets/close-button.png';
-import { getLatestNotification } from '../utils/util';
 import NotificationItem from './NotificationItem';
 
-function Notification() {
+const notificationsList = [
+    { id: 1, type: 'default', value: 'New course available' },
+    { id: 2, type: 'urgent', value: 'New resume available' },
+    { id: 3, type: 'urgent', html: { __html: 'Urgent requirement - complete by EOD' } },
+];
+
+function Notifications() {
     return (
-        <div className='Notifications'>
-            <button style={{
-                background: 'transparent',
-                position: 'absolute',
-                right: 20,
-                border: 'none'
-            }} aria-label='Close' onClick={console.log('Close button has been clicked')}><img src={closeIcon} alt='Close icon' /></button>
+        <div className="Notifications">
             <p>Here is the list of notifications</p>
             <ul>
-                <NotificationItem type="default" value="New course available" />
-                <NotificationItem type="urgent" value="New resume available" />
-                <NotificationItem type='urgent' html={getLatestNotification()} />
+                {notificationsList.map((notif) => (
+                    <NotificationItem
+                        key={notif.id}
+                        type={notif.type}
+                        value={notif.value}
+                        html={notif.html}
+                    />
+                ))}
             </ul>
         </div>
-    )
+    );
 }
 
-export default Notification;
+export default Notifications;
