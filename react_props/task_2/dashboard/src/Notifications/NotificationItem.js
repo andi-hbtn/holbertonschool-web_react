65f0287e570621fc095347fc1b324da03cf28685
andi-hbtn <andi.bevapi@holbertonschool.com>
2025-09-25 12:34:33 +0200

    changes for test

--- react_props/task_2/dashboard/src/Notifications/NotificationItem.js
+++ react_props/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function NotificationItem({ type, html, value }) {
-    if (value) {
-        return <li data-notification-type={type}>{value}</li>;
+function NotificationItem({ type, value, html }) {
+    if (html) {
+        return <li data-notification-type={type} dangerouslySetInnerHTML={html}></li>;
     }
-
-    return <li data-notification-type={type} dangerouslySetInnerHTML={{ __html: html }}></li>;
+    return <li data-notification-type={type}>{value}</li>;
 }
+
+NotificationItem.propTypes = {
+    type: PropTypes.string,
+    value: PropTypes.string,
+    html: PropTypes.shape({
+        __html: PropTypes.string,
+    }),
+};
+
+NotificationItem.defaultProps = {
+    type: 'default',
+    value: '',
+    html: null,
+};
 
 export default NotificationItem;
